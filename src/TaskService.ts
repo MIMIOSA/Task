88fa7bb0aab65776efe2733365e90eec9ad6ad03
ZHYR\昊赟睿 <sc2_lemon@163.com>
2016-11-23 12:52:21 +0800

    Finish Homework 8

--- src/TaskService.ts
+++ src/TaskService.ts
@@ -1,11 +1,13 @@
-
-class TaskService {
+class TaskService implements Observer {
 	observerList: ObserverType[];
 	taskList: Task[];
 	public constructor() {
 		this.observerList = new Array();
 		this.taskList = new Array();
 		this.addTask("001");
+		this.addTask("002");
+	}
+	onChange() {
 
 	}
 	task: Task;
@@ -17,19 +19,45 @@ class TaskService {
 			element.observer.onChange(task);
 		});
 	}
-
-	public finish(id: string): ErrorCode {
+	public canAccepted(id: string) {
+		var task: Task;
+		task = taskSearch(this.taskList, id);
+		switch (id) {
+			case "002":
+				task.status = 1;
+				this.Notify(task);
+				break;
+		}
+	}
+	public canFinished(id: string) {
+		var task: Task;
+		task = taskSearch(this.taskList, id);
+		switch (id) {
+			case "002":
+				task.status = 3;
+				this.Notify(task);
+				break;
+		}
+	}
+	public finished(id: string): ErrorCode {
 		var task: Task;
 		task = taskSearch(this.taskList, id);
 		switch (id) {
 			case "001":
 				task.status = 4;
 				this.Notify(task);
+				this.canAccepted("002");
+				break;
+
+			case "002":
+				task.status = 4;
+				this.Notify(task);
+				break;
 			default:
 				return ErrorCode.TASK_ERROR_UNFIND;
 		}
 	}
-	public accept(id: string): void {
+	public accepted(id: string): void {
 		var task: Task;
 		task = taskSearch(this.taskList, id);
 		switch (id) {
@@ -37,8 +65,12 @@ class TaskService {
 				task.status = TaskStatus.DURING;
 				this.Notify(task);
 				break;
+
+			case "002":
+				task.status = TaskStatus.DURING;
+				this.Notify(task);
+				break;
 			default:
-				console.log("Task cannot be found");
 		}
 	}
 	public during(id: string) {
@@ -46,6 +78,9 @@ class TaskService {
 		task = taskSearch(this.taskList, id);
 		switch (id) {
 			case "001":
+				this.Notify(task);
+
+			case "002":
 				this.Notify(task);
 			default:
 		}
@@ -60,21 +95,33 @@ class TaskService {
                 this.taskList.push(task);
 				this.Notify(task);
                 break;
+
+			case "002":
+                var task = new Task("002", "Task 2", "Kill 10 monster", 0, "npc_1", "npc_1");
+                this.taskList.push(task);
+				this.Notify(task);
+                break;
         }
 
 	}
 
-	getTaskByCustomRole(rule: Function, Id: string): Task {
+	getTask(rule: Function, Id: string): Task {
 		return rule(this.taskList, Id);
 
 	}
-	checkTaskRules(task: Task, npcId: string, NPCtalkpanel: DialoguePanel) {
+	checkTaskStatus(task: Task, npcId: string, DialoguePanel: DialoguePanel) {
         switch (task.status) {
             case TaskStatus.ACCEPTABLE:
                 switch (task.id) {
                     case "001":
                         if (task.fromNpcId == npcId) {
-							NPCtalkpanel.onOpen(task);
+							DialoguePanel.onOpen(task);
+							this.Notify(task);
+                        }
+                        break;
+					case "002":
+						if (task.fromNpcId == npcId) {
+							DialoguePanel.onOpen(task);
 							this.Notify(task);
                         }
                         break;
@@ -87,6 +134,11 @@ class TaskService {
                             this.Notify(task);
 
                         }
+					case "002":
+                        if (task.toNpcId == npcId) {
+							DialoguePanel.onOpen(task);
+                            this.Notify(task);
+                        }
                         break;
                 }
                 break;
@@ -96,7 +148,12 @@ class TaskService {
                         if (task.toNpcId == npcId) {
 
                             task.status = TaskStatus.CAN_SUBMIT;
-							NPCtalkpanel.onOpen(task);
+							DialoguePanel.onOpen(task);
+                            this.Notify(task);
+                        }
+                        break;
+					case "002":
+                        if (task.toNpcId == npcId) {
                             this.Notify(task);
                         }
                         break;
@@ -107,11 +164,17 @@ class TaskService {
                     case "001":
                         this.Notify(task);
                         break;
+					case "002":
+                        this.Notify(task);
+                        break;
                 }
                 break;
             case TaskStatus.UNACCEPTABLE:
                 switch (task.id) {
                     case "001":
+						this.Notify(task);
+                        break;
+					case "002":
 						this.Notify(task);
                         break;
                 }
@@ -126,18 +189,11 @@ function taskSearch(taskList: Task[], id: string): Task {
 			return taskList[i];
 		}
 		else {
-			console.log("task named" + id + "can not be found");
 		}
 	}
 
 }
 
-enum ErrorCode {
-
-    TASK_ERROR_NULL,
-    TASK_ERROR_UNFIND
-
-}
 class ObserverType {
 
 	observer: Observer;
@@ -149,3 +205,11 @@ class ObserverType {
 	}
 
 }
+
+enum ErrorCode {
+
+    TASK_ERROR_NULL,
+    TASK_ERROR_UNFIND
+
+}
+
