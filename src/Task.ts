bc8c54e739496b46f9de417f954f8e3f722a2507
ZHYR\昊赟睿 <sc2_lemon@163.com>
2016-11-23 12:55:43 +0800

    增加了TaskCondition和NPCTalkTaskCondition

--- src/Task.ts
+++ src/Task.ts
@@ -1,6 +1,27 @@
-class Observer{
-    onChange(task:Task){  
+class Observer {
+    onChange(task: Task) {
     }
+}
+
+class TaskCondition {
+    onAccept(task) {
+
+    }
+    onsubmit(task) {
+
+    }
+}
+
+class NPCTalkTaskCondition extends TaskCondition {
+    constructor(){
+        super();
+    }
+//    onAccept(task) {
+//
+//    }
+//    onsubmit(task) {
+//
+//    }
 }
 
 class Task {
@@ -11,12 +32,12 @@ class Task {
     fromNpcId: string;
     toNpcId: string;
     public constructor(id, name, desc, status, fromNpcId, toNpcId) {
-        this.id=id;
-        this.name=name;
-        this.desc=desc;
-        this.status=status;
-        this.fromNpcId=fromNpcId;
-        this.toNpcId=toNpcId;
+        this.id = id;
+        this.name = name;
+        this.desc = desc;
+        this.status = status;
+        this.fromNpcId = fromNpcId;
+        this.toNpcId = toNpcId;
     }
 }
 
